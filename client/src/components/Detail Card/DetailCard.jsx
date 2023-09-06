1dd745d525d1344634361c5ca205db11d2ef7972
soldier_lucas <3jhoncuco@gmail.com>
2023-09-06 11:18:02 -0300

    In progress

--- client/src/components/Detail Card/DetailCard.jsx
+++ client/src/components/Detail Card/DetailCard.jsx
@@ -5,31 +5,32 @@ import { deleteActivity, getCountryDetail } from "../Redux/actions";
 import style from './DetailCard.module.css'
 import deleteIcon from '../Images/deleteIcon.svg'
 import editIcon from '../Images/editIcon.svg'
-import Form from '../Form/Form'
 
 const DetailCard = () => {
     const { id } = useParams()
     const dispatch = useDispatch();
     const country = useSelector(state => state.countryDetail)
+
     useEffect(() => {
         dispatch(getCountryDetail(id))
     }, [])
-    
+
     const deleteActivityHandler = (ActivityId) => {
         dispatch(deleteActivity(ActivityId))
     }
 
-    const [window, setWindow] = useState(false)
-    const handlerWindow = () => {
-        setWindow(!window)
-    }
-    // const updateActivityHandler = (ActivityId) => {
+    // const [edit, setEdit] = useState()
 
+    // const handlerEdit = (ActivityId) => {
+    //     if (edit) {
+    //         setEdit()
+    //     } else {
+    //         setEdit(ActivityId)
+    //     }
     // }
 
     return (
         <div className={style.background}>
-            {window === true ? <Form></Form> : null}
             <div className={style.container}>
                 <div className={style.detail}>
                     <div className={style.detailFixed}>
@@ -44,14 +45,18 @@ const DetailCard = () => {
                     </div>
                 </div>
                 <div className={style.activities}>
-                    {country?.Activities?.map(countryActivity => {
+
+                    {country?.Activities?.map((countryActivity) => {
+
+                        const ActivityId = countryActivity.Countries_Activities.ActivityId
                         return (
                             <div key={countryActivity.Countries_Activities.ActivityId} id={countryActivity.Countries_Activities.ActivityId} className={style.cardActivity}>
                                 <div className={style.containerIcons}>
-                                    
-                                <img onClick={handlerWindow} className={style.editIcon} src={editIcon}/>
-                                <img onClick={() => deleteActivityHandler(countryActivity.Countries_Activities.ActivityId)} className={style.deleteIcon} src={deleteIcon}/>
+
+                                    <img className={style.editIcon} src={editIcon} />
+                                    <img onClick={() => deleteActivityHandler(countryActivity.Countries_Activities.ActivityId)} className={style.deleteIcon} src={deleteIcon} />
                                 </div>
+
                                 <h2>{countryActivity.name}</h2>
                                 <h3>Difficulty: {countryActivity.dificultad}</h3>
                                 <h3>{countryActivity.duracion} Hours</h3>
@@ -59,7 +64,9 @@ const DetailCard = () => {
                                     return (
                                         <h3 key={season}>{season}</h3>
                                     )
-                                })}
+                                })
+                                }
+
                             </div>
                         )
                     })}
