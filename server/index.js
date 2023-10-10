9fa835c1024a9e4ad1cfbdb0843d8f79049f3788
soldier_lucas <3jhoncuco@gmail.com>
2023-10-10 18:59:22 -0300

    fixed

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
 const postDataToDB = require('./src/utils/postDataToDB')
-const PORT = 3001;
+const { PORT } = process.env;
+
+const port = PORT || 3001;
+const host = "0.0.0.0";
 
 conn.sync({ force: false }).then(() => {
-server.listen(PORT, () => {
-  // postDataToDB();
-  console.log(`Server listening on port ${PORT}`);
-})
+  server.listen(port, host, () => {
+    postDataToDB();
+    console.log(`Server listening on port ${port}`);
+  })
 }).catch(error => console.error(error))
+
