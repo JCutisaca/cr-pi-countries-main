2a7bb4f42ac2134d4d2692648bf84f0159122003
soldier_lucas <3jhoncuco@gmail.com>
2023-10-10 14:54:12 -0300

    access server

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = require("./routes");
 const morgan = require("morgan");
-const cors = require("cors");
 
 const server = express();
 
-server.use(morgan("dev"));
 server.use(express.json());
+
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', 'true');
@@ -20,7 +19,7 @@ server.use((req, res, next) => {
     );
     next();
  });
-
+ 
 server.use(router);
 
 module.exports = server;
