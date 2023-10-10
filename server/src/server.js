e8bdb9b973af77ddfc0a1ef3c5a10b768e878c28
soldier_lucas <3jhoncuco@gmail.com>
2023-10-10 16:04:56 -0300

    fixed cors

--- server/src/server.js
+++ server/src/server.js
@@ -3,11 +3,15 @@ const router = require("./routes");
 const morgan = require("morgan");
 const cors = require('cors')
 
+const corsOptions = {
+    origin: 'https://travelsdreamscape.up.railway.app/'
+  }
+
 const server = express();
 
 server.use(morgan("dev"));
 server.use(express.json());
-server.use(cors());
+server.use(cors(corsOptions));
  
 server.use(router);
 
